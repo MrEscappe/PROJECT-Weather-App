2c4e696aca310b32621d5820c0ade632cb2eebc0
José Sérgio <Maverikxd1@gmail.com>
2024-04-01 10:19:00 -0300

    README updated with project details

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # PROJECT-Weather-App
+
+This project is part of the Odin Project Curriculum. The goal of this project is to build a weather app that allows users to search for a city and see the current weather conditions in that city. The app will display the city name, temperature, weather description, and an icon representing the weather conditions.  The app will use the weatherapi  to get the weather data for the city that the user searches for.
+
+## Built With
+
+<div align="center">
+
+- ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+- ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+- ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+</div>
+
+## Live Demo 
+
+[Live Demo Link](https://mrescappe.github.io/PROJECT-Weather-App/)
+
+
+## What I learned
+
+- How to use the fetch API to make requests to a third-party API.
+- How to use the async/await syntax to handle promises.
+- How to use the DOM to update the content of a webpage.
+
+- Copyrigth (c) 2024 [José Sérgio](https://github.com/MrEscappe)
---
+++ script/script.js
@@ -0,0 +1,120 @@
+const searchBar = document.querySelector(".search-bar");
+const searchBtn = document.querySelector(".search-button");
+
+const locationElement = document.querySelector(".city-name");
+const weatherElement = document.querySelector(".weather");
+const temperatureElement = document.querySelector(".temperature");
+const weatherIcon = document.querySelector(".weather-icon");
+const rainElement = document.querySelector(".rain");
+
+const weatherDay1 = document.querySelector(".weather-day1");
+const weatherDay2 = document.querySelector(".weather-day2");
+const weatherDay3 = document.querySelector(".weather-day3");
+
+const temperatureDay1 = document.querySelector(".temperature-day1");
+const temperatureDay2 = document.querySelector(".temperature-day2");
+const temperatureDay3 = document.querySelector(".temperature-day3");
+
+const weatherIconDay1 = document.querySelector(".weather-icon-day1");
+const weatherIconDay2 = document.querySelector(".weather-icon-day2");
+const weatherIconDay3 = document.querySelector(".weather-icon-day3");
+
+const dayElements = document.querySelectorAll(".day");
+
+const API_KEY = "7526f720c6a5461c8ef101027242103";
+
+searchBar.addEventListener("keyup", (e) => {
+	if (e.key === "Enter") {
+		searchCity(e.target.value);
+	}
+});
+
+searchBtn.addEventListener("click", () => {
+	if (searchBar.value) {
+		searchCity(searchBar.value);
+	}
+
+	searchBar.classList.toggle("active");
+	if (searchBar.classList.contains("active")) {
+		searchBar.focus();
+	} else {
+		searchBar.blur();
+	}
+});
+
+// Fetch weather data from API and display it ('http://api.weatherapi.com/v1')
+
+async function searchCity(city = "São Paulo") {
+	try {
+		const response = await fetch(`
+        http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&lang=pt
+        `);
+		const data = await response.json();
+
+		displayWeather(data);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function displayWeather(data) {
+	const { name } = data.location;
+	const { icon, text: description } = data.current.condition;
+	const { temp_c, temp_f } = data.current;
+	const { forecastday } = data.forecast;
+
+	displayForecast(data.forecast.forecastday);
+	isGoingToRain(forecastday);
+	locationElement.textContent = name;
+	weatherElement.textContent = description;
+	weatherIcon.src = `http:${icon}`;
+	weatherIcon.alt = description;
+	temperatureElement.textContent = `${temp_c}°C`;
+}
+
+function isGoingToRain(forecastday) {
+	if (forecastday[0].day.totalprecip_mm > 0) {
+		rainElement.textContent = "Vai chuve!";
+	} else {
+		rainElement.textContent = "Não vai chuve!";
+	}
+}
+
+function displayForecast(forecastday) {
+	console.log(forecastday);
+	dayElements.forEach((day, index) => {
+		if (index === 0) {
+			let day = forecastday[index].date;
+			day = day.split("-");
+			day = `${day[2]}/${day[1]}`;
+			dayElements[index].textContent = day;
+
+			weatherDay1.textContent = forecastday[index].day.condition.text;
+			temperatureDay1.textContent = `${forecastday[index].day.maxtemp_c}°C`;
+			weatherIconDay1.src = `http:${forecastday[index].day.condition.icon}`;
+			weatherIconDay1.alt = forecastday[index].day.condition.text;
+		} else if (index === 1) {
+			let day = forecastday[index].date;
+			day = day.split("-");
+			day = `${day[2]}/${day[1]}`;
+			dayElements[index].textContent = day;
+
+			weatherDay2.textContent = forecastday[index].day.condition.text;
+			temperatureDay2.textContent = `${forecastday[index].day.maxtemp_c}°C`;
+			weatherIconDay2.src = `http:${forecastday[index].day.condition.icon}`;
+			weatherIconDay2.alt = forecastday[index].day.condition.text;
+		} else if (index === 2) {
+			let day = forecastday[index].date;
+			day = day.split("-");
+			day = `${day[2]}/${day[1]}`;
+			dayElements[index].textContent = day;
+
+			weatherDay3.textContent = forecastday[index].day.condition.text;
+			temperatureDay3.textContent = `${forecastday[index].day.maxtemp_c}°C`;
+			weatherIconDay3.src = `http:${forecastday[index].day.condition.icon}`;
+			weatherIconDay3.alt = forecastday[index].day.condition.text;
+		}
+	});
+}
+
+searchCity();
